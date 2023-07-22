cf6faaecc37267a7272e3993cfca2cb3456bb1f3
Dulux-Oz <matthew@peregrineit.net>
2023-07-22 18:27:22 +1000

    Fixed License Block In Files - MJB - 20230722a
    Removed license block from files

--- scripts/dlc-settings.js
+++ scripts/dlc-settings.js
@@ -1,22 +1,3 @@
-/*
-################################################################################
-## Copyright (c) 2023 {{Andrew}}, {{Andy}}, {{Ben}}, Matthew J Black,         ##
-##    {{Haxxx}}  & {{James}}.                                                 ##
-## This file is part of the *Deadlands Classic* Foundry Virtual Tabletop Game ##
-##   System.                                                                  ##
-## *Deadlands Classic* is free software: you can redistribute it and/or       ##
-##    modify it under the terms of the GNU General Public License as          ##
-##    published by the Free Software Foundation, either version 3 of the      ##
-##    License, or (at your option) any later version.                         ##
-## *Deadlands Classic* is distributed in the hope that it will be useful, but ##
-##    WITHOUT ANY WARRANTY; without even the implied warranty of              ##
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        ##
-##    General Public License for more details.                                ##
-## You should have received a copy of the GNU General Public License along    ##
-##    with *Deadlands Classic*. If not, see <https://www.gnu.org/licenses/>.  ##
-################################################################################
-*/
-
 "use strict";
 
 export function fpCreateGameSettings() {

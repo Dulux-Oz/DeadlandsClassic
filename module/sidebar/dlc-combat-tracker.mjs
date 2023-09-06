10e30e7926e38319d953fe72bd51fe8d8714b699
Andrew Wilson <andrew@rivendale.net>
2023-09-06 01:58:04 +0100

    Tweaks to the html
    trying to get the combat tracker html to do sensible things.

--- module/sidebar/dlc-combat-tracker.mjs
+++ module/sidebar/dlc-combat-tracker.mjs
@@ -10,4 +10,41 @@ export class DeadlandsCombatTracker extends CombatTracker {
       { overwrite: true }
     );
   }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async getData(options = {}) {
+    let context = await super.getData(options);
+
+    if (this.viewed !== null && typeof this.viewed !== 'undefined') {
+      const roundStarted =
+        this.viewed.roundStarted != null &&
+        typeof this.viewed.roundStarted !== 'undefined'
+          ? !!this.viewed.roundStarted
+          : false;
+
+      const offerEndTurn =
+        this.viewed.offerEndTurn != null &&
+        typeof this.viewed.offerEndTurn !== 'undefined'
+          ? !!this.viewed.offerEndTurn
+          : false;
+
+      context = foundry.utils.mergeObject(context, {
+        roundStarted,
+        offerEndTurn,
+      });
+    }
+
+    for (let index = 0; index < context.turns.length; index += 1) {
+      const element = context.turns[index];
+
+      // It's always the first entry that active in deadlands classic
+      element.active = index === 0;
+      element.hasRolled = false;
+      element.roundStarted = this.viewed.roundStarted;
+    }
+
+    return context;
+  }
 }
